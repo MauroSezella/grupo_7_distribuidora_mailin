591914a5cdf74aec130dbd536b3610f665b9843d
diegoa1501 <diegoa1501@gmail.com>
2023-11-07 04:56:43 -0300

    modificado archivo app.js para incluir ruta a archivo html(comentado)

--- app.js
+++ app.js
@@ -6,4 +6,6 @@ app.listen(3030, () => console.log('Servidor Corriendo'));
 
 app.use(express.static('public'));
 
+//app.get('/register', (req,res) => res.sendFile(path.resolve("./views/register.html")));
+
 //app.get('/',(req,res) => res.sendFile(path.resolve("./views/index.html")));
