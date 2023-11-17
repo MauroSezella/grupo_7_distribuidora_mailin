7f6cc9f2110f777daae351f89f60ac80526155fb
Tade0G <tadeogomez1988@gmail.com>
2023-11-16 23:58:56 -0300

    agregando todo de golpe

--- app.js
+++ app.js
@@ -10,3 +10,5 @@ app.get('/',(req,res) => res.sendFile(path.resolve("./views/index.html")));
 app.get('/login',(req,res) => res.sendFile(path.resolve("./views/login.html")));
 app.get('/productDetail',(req,res) => res.sendFile(path.resolve("./views/productDetail.html")));
 app.get('/productCart',(req,res) => res.sendFile(path.resolve("./views/productCart.html")));
+app.get('/register',(req,res) => res.sendFile(path.resolve("./views/register.html")));
+
