ca317938d82d4fdedf899fd0acdaf2fea519a6a3
MauroSezella <maurosezella@gmail.com>
2023-12-04 23:56:02 -0300

    elimino rutas sin usar

--- src/app.js
+++ src/app.js
@@ -14,16 +14,3 @@ app.set('views', path.join(__dirname, '/views'));
 app.use('/', indexRouter);
 
 
-
-
-
-app.get('/formProduct',(req,res) => res.sendFile(path.resolve(__dirname, 'views/products/productForm.html')));
-
-
-
-/*app.get('/',(req,res) => res.sendFile(path.resolve("./views/index.html")));
-app.get('/login',(req,res) => res.sendFile(path.resolve("./views/login.html")));
-app.get('/productDetail',(req,res) => res.sendFile(path.resolve("./views/productDetail.html")));
-app.get('/productCart',(req,res) => res.sendFile(path.resolve("./views/productCart.html")));
-app.get('/register',(req,res) => res.sendFile(path.resolve("./views/register.html")));*/
-
