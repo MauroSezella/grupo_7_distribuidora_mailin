c7dacb448744f4c54a031ac1272d55b7d77a78a6
MauroSezella <maurosezella@gmail.com>
2023-12-15 18:20:25 -0300

    Agregamos middlewares al app.js

--- src/app.js
+++ src/app.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const methodOverride =  require('method-override');
 
 const indexRouter = require('./routes/index.routes');
 
 app.listen(3030, ()=> console.log('Servidor corriendo en el puerto 3030'));
 
+//Middlewares
 app.use(express.static(path.resolve(__dirname,'../public')));
+app.use(express.json());
+app.use(methodOverride('_method'));
 
+//Template Engine 
 app.set('view engine','ejs');
 app.set('views', path.join(__dirname, '/views'));
 
