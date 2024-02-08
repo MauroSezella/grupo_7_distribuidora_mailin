e74bfc8eec553a93b0e6c3ea153dfdf8c88534c2
Frenchok <fedecoronel@hotmail.com>
2024-02-08 20:20:59 -0300

    agregamos modelos de usuarios

---
+++ src/model/database/models/Usuario.js
@@ -0,0 +1,30 @@
+let modelUsuarios = (sequelize, DataTypes) => {
+    let alias = "Usuarios";
+    let columns = {
+        id : {
+            type : DataTypes.INTEGER , 
+            allowNull : false ,
+            primaryKey : true ,
+            autoIncrement : true ,
+        },
+        nombre : DataTypes.STRING ,
+        apellido : DataTypes.STRING ,
+        email : DataTypes.STRING ,
+        password : DataTypes.STRING ,
+        avatar : DataTypes.STRING ,
+        rol : DataTypes.STRING ,
+        estado : DataTypes.INTEGER ,
+    };
+    let config = {
+        tableName : "usuarios",
+        timestamps : false
+    };
+    let Usuario = sequelize.define(alias, columns, config);
+
+
+    
+    
+        return Usuario
+}
+
+module.exports = modelUsuario ;
