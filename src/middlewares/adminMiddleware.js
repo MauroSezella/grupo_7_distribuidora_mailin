276badaeecbadafa2dd54fa54f9b5e6059b15978
lucianabell <lucianabellgarcia@gmail.com>
2024-02-26 21:40:34 -0300

    corrijo adminMiddleware

--- src/middlewares/adminMiddleware.js
+++ src/middlewares/adminMiddleware.js
@@ -1,5 +1,5 @@
 function adminMiddleware (req, res, next){
-    if (!req.session.userLogged || req.session.userLogged.rol != 'admin'){
+    if (!req.session.userLogged || req.session.userLogged.rol != 'ADMIN'){
         return res.redirect('/')
     }
     next()
