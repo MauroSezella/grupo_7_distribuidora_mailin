c7b94ce475e69e80641b86ae798900c84900c557
lucianabell <lucianabellgarcia@gmail.com>
2024-02-26 23:58:48 -0300

    corrijo vista de admin

--- src/model/services/productService.js
+++ src/model/services/productService.js
@@ -1,6 +1,5 @@
 const fs = require("fs");
 const path = require("path");
-const User = require("../../data/userService");
 const db = require('../database/models');
 const { Op } = require("sequelize");
 
