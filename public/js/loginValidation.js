0aed969ca31f267c67067bc600a3fe341e055da3
lucianabell <lucianabellgarcia@gmail.com>
2024-03-13 13:04:13 -0300

    fix en validaciones login y register

--- public/js/loginValidation.js
+++ public/js/loginValidation.js
@@ -1,41 +1,68 @@
 window.addEventListener('load', function () {
     let btnSubmit = document.getElementById('btnSubmit');
-    
+
     let inputEmail = document.getElementById('email');
     let inputPassword = document.getElementById('password');
-    
-    
+
+
     let erEmail = document.getElementById('erEmail');
     let erPassword = document.getElementById('erPassword');
-   
-    let registerForm = document.getElementById('form');
+
+    let LoginForm = document.getElementById('form');
 
     const validacion = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    
-   
 
-    btnSubmit.addEventListener('click', function (e) {
-        
-        e.preventDefault();
-        let errores = {};   
-        
-        if (inputEmail.value.length < 1 ||   !validacion.test(inputEmail.value) ) {
-            errores.email = 'Debe ingresar un email válido registrado previamente';
-        }
-
-        if (inputPassword.value.length < 8) {
-            errores.password = 'La contraseña debe tener al menos 8 caracteres';
-        }
-
-        
-        
-        if (Object.keys(errores).length >= 1) {
-            
-            erEmail.innerText = (errores.email) ? errores.email : "";
-            erPassword.innerText = (errores.password) ? errores.password : "";
-            
-        } else {
-            registerForm.submit();
+    let erEmailBack = document.getElementById('erEmail-back');
+    inputEmail.addEventListener('input', function () {
+        if (erEmailBack.innerHTML !== '') {
+            erEmailBack.innerHTML = '';
+            inputEmail.classList.remove('is-invalid')
         }
     });
+
+    btnSubmit.addEventListener('click', function (e) {
+
+        e.preventDefault();
+        let errores = {};
+
+        if (!validacion.test(inputEmail.value)) {
+            errores.email = 'Debe ingresar un email válido';
+            inputEmail.classList.add('is-invalid')
+        } else{
+            inputEmail.classList.remove('is-invalid')
+        }
+
+        if (inputPassword.value.trim()=='') {
+            errores.password = 'Tienes que escribir una contraseña';
+            inputPassword.classList.add('is-invalid')
+        }else{
+            inputPassword.classList.remove('is-invalid')
+        }
+
+        if (Object.keys(errores).length >= 1) {
+            erEmail.innerText = (errores.email) ? errores.email : "";
+            erPassword.innerText = (errores.password) ? errores.password : "";
+        } else {
+            LoginForm.submit();
+        }
+
+    });
+
+
+    let mostrarPassword = document.getElementById('mostrarPassword')
+
+    mostrarPassword.addEventListener('click', ()=>{
+        if (inputPassword.type === 'password') {
+            inputPassword.type = 'text';
+            mostrarPassword.classList.remove('fa-eye-slash');
+            mostrarPassword.classList.add('fa-eye');
+        } else {
+            inputPassword.type = 'password';
+            mostrarPassword.classList.remove('fa-eye');
+            mostrarPassword.classList.add('fa-eye-slash');
+        }
+    })
+
+
+
 });
--- public/js/registerValidation.js
+++ public/js/registerValidation.js
@@ -6,55 +6,99 @@ window.addEventListener('load', function () {
     let inputPassword = document.getElementById('password');
     let inputConfirmar = document.getElementById('confirmPassword')
     let inputArchivo = document.getElementById('avatar');
+    let inputTerminos = document.getElementById('aceptaTerminos');  
+    
     let erNombre = document.getElementById('erNombre');
     let erApellido = document.getElementById('erApellido');
     let erEmail = document.getElementById('erEmail');
     let erPassword = document.getElementById('erPassword');
     let erConfirmar = document.getElementById('erConfirmar');
-    let erArchivo = document.getElementById('erArchivo');
+    let erArchivo = document.getElementById('erArchivo');  
+    let erTerminos = document.getElementById('erAcepTerminos');  
     let registerForm = document.getElementById('form');
-
+    
     const validacion = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-   
+    
     const acceptedExtensions = ['jpg', 'png', 'jpeg', 'gif'];
     const esImagenValida = (val) => {
         if (val) {
             const array = val.split(".")
             const lastItem = array.pop()
-           return acceptedExtensions.includes(lastItem)  
+            return acceptedExtensions.includes(lastItem)
         }
-   }
-
+    }
+    
+    const erEmailBack = document.getElementById('erEmail-back');
+    inputEmail.addEventListener('input', function () {
+        if (erEmailBack.innerHTML !== '') {
+            erEmailBack.innerHTML = '';
+            inputEmail.classList.remove('is-invalid')
+        }
+    });
+    
     btnSubmit.addEventListener('click', function (e) {
         
         e.preventDefault();
         let errores = {};
 
-        
 
-        if (inputNombre.value.length < 4) {
-            errores.nombre = 'El nombre debe tener al menos 4 caracteres';
+        if (inputNombre.value.trim().length < 2) {
+            errores.nombre = 'El nombre debe tener al menos 2 caracteres';
+            inputNombre.classList.add('is-invalid')
+        }else{
+            inputNombre.classList.remove('is-invalid')
         }
 
-        if (inputApellido.value.length < 4) {
-            errores.apellido = 'El apellido debe tener al menos 4 caracteres';
+        if (inputApellido.value.length < 2) {
+            errores.apellido = 'El apellido debe tener al menos 2 caracteres';
+            inputApellido.classList.add('is-invalid')
+        }else{
+            inputApellido.classList.remove('is-invalid')
         }
 
-        if (inputEmail.value.length < 1 ||  !validacion.test(inputEmail.value) ) {
-            errores.email = 'Debe ingresar un email válido que no esté registrado previamente';
+        if (!validacion.test(inputEmail.value)) {
+            errores.email = 'Debe ingresar un email válido';
+            inputEmail.classList.add('is-invalid')
+        } else {
+            inputEmail.classList.remove('is-invalid')
         }
 
-        if (inputPassword.value.length < 8) {
+
+        if (inputConfirmar.value == '') {
+            errores.confirmPassword = 'Debes confirmar tu contraseña';
+            inputConfirmar.classList.add('is-invalid');
+           
+            }
+     
+        if (inputPassword.value.length >= 8) {
+            if (inputConfirmar.value !== '') {
+                if (inputConfirmar.value !== inputPassword.value) {
+                    errores.confirmPassword = 'Las contraseñas no coinciden';
+                    inputConfirmar.classList.add('is-invalid');
+                    inputPassword.classList.add('is-invalid');
+                } else {
+                    inputConfirmar.classList.remove('is-invalid');
+                    inputPassword.classList.remove('is-invalid');
+                }
+            } else {
+                errores.confirmPassword = 'Debes confirmar tu contraseña';
+                inputConfirmar.classList.add('is-invalid');
+            }
+        } else {
             errores.password = 'La contraseña debe tener al menos 8 caracteres';
+            inputPassword.classList.add('is-invalid');
+        }
+ 
+        if (inputArchivo.value && !esImagenValida(inputArchivo.value)) {
+            errores.archivo = `Las extensiones de archivo permitidas: ${acceptedExtensions.join(', ')}`
+            inputArchivo.classList.add('is-invalid')
+        } else {
+            inputArchivo.classList.remove('is-invalid')
         }
 
-        if (inputConfirmar.value !== inputPassword.value) {
-            errores.confirmPassword = 'Las contraseñas no coinciden';
+        if (!inputTerminos.checked) {
+            errores.terminos = 'Debe aceptar los términos y condiciones';
         }
-    
-        if (inputArchivo.value && !esImagenValida(inputArchivo.value)) {
-                errores.archivo = `Las extensiones de archivo permitidas: ${acceptedExtensions.join(', ')}`
-         }
 
         console.log(errores, erNombre)
         if (Object.keys(errores).length >= 1) {
@@ -64,6 +108,7 @@ window.addEventListener('load', function () {
             erPassword.innerText = (errores.password) ? errores.password : "";
             erConfirmar.innerText = (errores.confirmPassword) ? errores.confirmPassword : "";
             erArchivo.innerText = (errores.archivo) ? errores.archivo : "";
+            erTerminos.innerText = (errores.terminos) ? errores.terminos : "";
         } else {
             registerForm.submit();
         }
