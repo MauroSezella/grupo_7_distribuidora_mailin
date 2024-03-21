ffea2a79c08404814848b885b983e53f89f7a3e8
diegoa1501 <diegoa1501@gmail.com>
2024-03-21 12:31:45 -0300

    agregamos el uso de la ruta de api

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -5,11 +5,13 @@ const path = require('path');
 const routerProduct = require('./product.routes');
 const routerMain = require('./main.routes');
 const routerUser = require('./user.routes');
+const routerApi = require('./api/api.routes');
 
 
 router.use('/', routerMain);
 router.use('/user', routerUser);
 router.use('/productos', routerProduct);
+router.use('/api', routerApi);
 
 
 module.exports = router;
